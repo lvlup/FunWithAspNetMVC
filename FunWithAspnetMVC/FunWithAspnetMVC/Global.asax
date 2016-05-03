200b25cb5c49adfee9f9b06dff2bc48f49ece276
ruslan <qwe@gmail.com>
2016-05-03 15:36:25 +0200

    Init app

---
+++ FunWithAspnetMVC/FunWithAspnetMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FunWithAspnetMVC.MvcApplication" Language="C#" %>
---
+++ FunWithAspnetMVC/FunWithAspnetMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FunWithAspnetMVC.Startup))]
+namespace FunWithAspnetMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
