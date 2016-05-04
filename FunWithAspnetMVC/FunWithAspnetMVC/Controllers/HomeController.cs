75baa3a77b3edb61124869c887a0e1d33e17d453
ruslan <qwe@gmail.com>
2016-05-04 21:00:22 +0200

    Добавил поиск по книгам

--- FunWithAspnetMVC/FunWithAspnetMVC/Controllers/HomeController.cs
+++ FunWithAspnetMVC/FunWithAspnetMVC/Controllers/HomeController.cs
@@ -3,13 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using FunWithAspnetMVC.DAL;
 
 namespace FunWithAspnetMVC.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        private LibraryContext db = new LibraryContext();
+
+        public ViewResult Index(string searchString)
         {
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                var searchingBooks = db.Books.Where(b => b.Genre.Contains(searchString)
+                                                         || b.Name.Contains(searchString)
+                                                         || b.Writer.FirstName.Contains(searchString)
+                                                         || b.Writer.LastName.Contains(searchString)).ToList();
+                return View(searchingBooks);
+            }
+
             return View();
         }
 
